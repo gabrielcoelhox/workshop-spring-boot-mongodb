020bf2cded7597746504852a263abe7840e4aa67
Gabriel Coelho <gabriel.coelhox@gmail.com>
2021-11-10 17:33:50 -0300

    Instanciacao da base de dados

---
+++ src/main/java/com/gabrielcoelho/workshopmongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.gabrielcoelho.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.gabrielcoelho.workshopmongo.domain.User;
+import com.gabrielcoelho.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();  //Limpa a coleção no mongoDB
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
