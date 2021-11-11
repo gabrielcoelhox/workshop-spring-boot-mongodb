a72cb85857ae5b91c3bd4f25da7678a0df9b06e6
Gabriel Coelho <gabriel.coelhox@gmail.com>
2021-11-11 17:37:02 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/gabrielcoelho/workshopmongo/domain/Post.java
+++ src/main/java/com/gabrielcoelho/workshopmongo/domain/Post.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.gabrielcoelho.workshopmongo.dto.AuthorDTO;
+
 @Document
 public class Post implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -16,17 +18,17 @@ public class Post implements Serializable{
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO author;
 	
 	public Post() {
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		this.id = id;
 		this.date = date;
 		this.title = title;
 		this.body = body;
-		this.author = author;
+		this.author = author;	
 	}
 
 	public String getId() {
@@ -61,11 +63,11 @@ public class Post implements Serializable{
 		this.body = body;
 	}
 	
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
