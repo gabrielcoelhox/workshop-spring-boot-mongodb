a55fbc45ad54c7226a0bd668b2e78ab106fae4bc
Gabriel Coelho <gabriel.coelhox@gmail.com>
2021-11-12 17:01:46 -0300

    Consulta simples com query methods

--- src/main/java/com/gabrielcoelho/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/gabrielcoelho/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.gabrielcoelho.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.gabrielcoelho.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	List<Post> findByTitleContainingIgnoreCase(String text); //Consulta
 }
