78397f76c421ae2b99bdae6f0deb59c17f8cdb79
Gabriel Coelho <gabriel.coelhox@gmail.com>
2021-11-12 16:30:56 -0300

    Obtendo um post por id

---
+++ src/main/java/com/gabrielcoelho/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.gabrielcoelho.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielcoelho.workshopmongo.domain.Post;
+import com.gabrielcoelho.workshopmongo.repository.PostRepository;
+import com.gabrielcoelho.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
